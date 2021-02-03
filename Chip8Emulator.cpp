9d9da58d8b48fc36c189ece636ec3c0aae3ae543
Armando Partida Sanabia <armandopartida0@gmail.com>
2021-02-03 10:45:02 -0500

    Fixup opcode() and other changes * Whoops. Fixed opcodes to make sure its 16 bits...not 8 bits * Added program counter code to move on to next instruction

--- Chip8Emulator.cpp
+++ Chip8Emulator.cpp
@@ -1,20 +1,20 @@
 // Chip8Emulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "Chippy.h"
 
 int main()
 {
+	// Verifying number stuff
+	/*uint16_t number = 0x8232;
 
+	uint16_t lowByte = number << 12;
+	uint16_t highByte = number & 0xF000;
+
+	if (highByte == 0x8000)
+	{
+		std::cout << "True" << std::endl;
+	}*/
+
+	Chippy* cpuTest = new Chippy();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Chippy.cpp
+++ Chippy.cpp
@@ -3,18 +3,28 @@
 Chippy::Chippy()
 {
 	// initialize stuff here
+	PC = 0x200;
+	I = 0;
+
+	for (int i = 0; i < 4096; i++)
+	{
+		MEMORY[i] = 0;
+	}
 }
 
 void Chippy::opcode()
 {
-	uint16_t highByte = PC & 0xF000;
-	switch (highByte)
+	// Reference: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM 
+	// All instructions are two bytes. So increase PC by 2 unless specified
+	uint16_t instruction = MEMORY[PC] << 8 | MEMORY[PC + 1];
+	switch (instruction & 0xF000)
 	{
 	case 0x0000:
 	{
-		switch (PC)
+		switch (instruction)
 		{
 		case 0x00E0: // Clear the display.
+			PC += 2;
 			break;
 		case 0x00EE: // Return from a subroutine.
 			PC = STACK.top();
@@ -24,39 +34,86 @@ void Chippy::opcode()
 		break;
 	}
 	case 0x1000: // Jump to location nnn.
-		PC = PC & 0x0FFF;
+		PC = instruction & 0x0FFF;
 		break;
 	case 0x2000: // Call subroutine at nnn.
 		STACK.push(PC);
-		PC = PC & 0x0FFF;
+		PC = instruction & 0x0FFF;
 		break;
 	case 0x3000: // Skip next instruction if Vx = kk.
-		if (V[(PC >> 8) & 0x000F] == (PC & 0x00FF))
+		if (V[(instruction >> 8) & 0x000F] == (instruction & 0x00FF))
+		{
+			PC += 4;
+		}
+		else
 		{
 			PC += 2;
 		}
 		break;
 	case 0x4000: // Skip next instruction if Vx != kk.
-		if (V[(PC >> 8) & 0x000F] != (PC & 0x00FF))
+		if (V[(instruction >> 8) & 0x000F] != (instruction & 0x00FF))
+		{
+			PC += 4;
+		}
+		else
 		{
 			PC += 2;
 		}
 		break;
 	case 0x5000: // Skip next instruction if Vx = Vy.
-		if (V[(PC >> 8) & 0x000F] == V[(PC >> 4) & 0x000F])
+		if (V[(instruction >> 8) & 0x000F] == V[(instruction >> 4) & 0x000F])
+		{
+			PC += 4;
+		}
+		else
 		{
 			PC += 2;
 		}
 		break;
 	case 0x6000: // Set Vx = kk.
-		V[(PC >> 8) & 0x000F] = PC & 0x00FF;
+		V[(instruction >> 8) & 0x000F] = instruction & 0x00FF;
 		break;
 	case 0x7000: // Set Vx = Vx + kk.
-		V[(PC >> 8) & 0x000F] += PC & 0x00FF;
+		V[(instruction >> 8) & 0x000F] += instruction & 0x00FF;
 		break;
-	case 0x8000:
+	case 0x8000: // 0x8xy0 - 0x8xy7, 0x8xyE
 	{
+		uint16_t lowByte = MEMORY[PC] << 12;
 
+		switch (lowByte)
+		{
+		case 0x0000: // Set Vx = Vy.
+			V[(instruction >> 8) & 0x000F] = V[(instruction >> 4) & 0x000F];
+			break;
+		case 0x1000: // Set Vx = Vx OR Vy.
+			V[(instruction >> 8) & 0x000F] = V[(instruction >> 8) & 0x000F] | V[(instruction >> 4) & 0x000F];
+			break;
+		case 0x2000: // Set Vx = Vx AND Vy.
+			V[(instruction >> 8) & 0x000F] = V[(instruction >> 8) & 0x000F] & V[(instruction >> 4) & 0x000F];
+			break;
+		case 0x3000: // Set Vx = Vx XOR Vy.
+			V[(instruction >> 8) & 0x000F] = V[(instruction >> 8) & 0x000F] ^ V[(instruction >> 4) & 0x000F];
+			break;
+		case 0x5000: // Set Vx = Vx - Vy, set VF = NOT borrow.
+			if (V[(instruction >> 8) & 0x000F] > V[(instruction >> 4) & 0x000F])
+			{
+				V[0xF] = 1;
+			}
+			else
+			{
+				V[0xF] = 0;
+			}
+
+			V[(instruction >> 8) & 0x000F] -= V[(instruction >> 4) & 0x000F];
+			break;
+		}
+
+		PC += 2;
+
+		break;
 	}
+
+	default:
+		std::cout << "Instruction: " << std::hex << MEMORY[PC] << " not implemented." << std::endl;
 	}
 }
