2af6712709343dcefb1950ea3428f4a1689c7e7c
Armando Partida-Sanabia <armandopartida0@outlook.com>
2023-11-15 19:02:25 -0700

    Refactor for upcoming changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 #include <memory>
 
-#include "SDL.h"
+// #include "SDL.h"
 
-#include "chippy_cpu.h"
-#include "chippy_display.h"
-#include "chippy_input.h"
+// #include "chippy_cpu.h"
+// #include "chippy_display.h"
+// #include "chippy_input.h"
 
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
 		std::cout << "usage: chippy <program> \n";
-		return 1;
+		return 0;
 	}
 
 	/* Try to open file */
@@ -26,9 +26,9 @@ int main(int argc, char **argv)
 	}
 
 	/* Initialize */
-	std::unique_ptr<ChippyCpu> cpu = std::make_unique<ChippyCpu>();
-	std::unique_ptr<ChippyInput> input = std::make_unique<ChippyInput>();
-	std::unique_ptr<ChippyDisplay> display = std::make_unique<ChippyDisplay>();
+	// std::unique_ptr<ChippyCpu> cpu = std::make_unique<ChippyCpu>();
+	// std::unique_ptr<ChippyInput> input = std::make_unique<ChippyInput>();
+	// std::unique_ptr<ChippyDisplay> display = std::make_unique<ChippyDisplay>();
 
 	/* Load file into temp buffer */
 	std::streampos size = file.tellg();
@@ -38,39 +38,39 @@ int main(int argc, char **argv)
 	file.close();
 
 	/* Load temp buffer into memory */
-	cpu->LoadProgram(buffer, size);
+	// cpu->LoadProgram(buffer, size);
 
 	/* Cleanup */
 	delete[] buffer;
 	
 	/* Loop will try to run at 60 FPS, not accurate because of sleep */
-	bool quit = false;
-	SDL_Event e;
-	Uint32 time_per_frame = 1000 / 60;
-	while (true)
-	{
-		auto start_time = SDL_GetTicks();
+	// bool quit = false;
+	// SDL_Event e;
+	// Uint32 time_per_frame = 1000 / 60;
+	// while (true)
+	// {
+	// 	auto start_time = SDL_GetTicks();
 
-		while (SDL_PollEvent(&e) != 0)
-		{
-			if (e.type == SDL_QUIT)
-			{
-				return 0;
-			}
-		}
+	// 	while (SDL_PollEvent(&e) != 0)
+	// 	{
+	// 		if (e.type == SDL_QUIT)
+	// 		{
+	// 			return 0;
+	// 		}
+	// 	}
 
-		/* Roughly 8 instructions per frame */
-		for(auto i = 0; i < 8; i++)
-		{
-			cpu->SetKeyboardState(input->GetInput());
-			cpu->Opcode();
-		}
+	// 	/* Roughly 8 instructions per frame */
+	// 	for(auto i = 0; i < 8; i++)
+	// 	{
+	// 		cpu->SetKeyboardState(input->GetInput());
+	// 		cpu->Opcode();
+	// 	}
 
-		auto end_time = SDL_GetTicks();
-		auto delta_time = end_time - start_time;
-		SDL_Delay(time_per_frame - delta_time);
+	// 	auto end_time = SDL_GetTicks();
+	// 	auto delta_time = end_time - start_time;
+	// 	SDL_Delay(time_per_frame - delta_time);
 
-		cpu->UpdateTimers();
-		display->Draw(cpu->GetDisplayBuffer());
-	}
+	// 	cpu->UpdateTimers();
+	// 	display->Draw(cpu->GetDisplayBuffer());
+	// }
 }
